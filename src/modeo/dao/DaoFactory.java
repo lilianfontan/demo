5fb3d63a6e597f59aa29277e94ceb17f9d8b06d3
lilianfontan <lilian.fontan@gmail.com>
2021-12-20 17:52:43 -0300

    findById implementation

--- src/modeo/dao/DaoFactory.java
+++ src/modeo/dao/DaoFactory.java
@@ -1,11 +1,12 @@
 package modeo.dao;
 
+import db.DB;
 import modeo.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 	
 	public static SellerDao createSellerDao() {
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(DB.getConnection());
 	}
 
 }
